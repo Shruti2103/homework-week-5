75bab8bcb0e813cc6d0293061d1f4e35861b40c6
Shruti2103 <shurtidevani21@gmail.com>
2022-03-05 17:00:18 +0000

    blabal

---
+++ src/homework/BioData.java
@@ -0,0 +1,16 @@
+package homework;
+//create BioData class
+public class BioData {
+
+    public static void main(String [] args){
+        System.out.println("Name: Komal Jimulia");
+        System.out.println("Date of Birth:24-10-1986");
+        System.out.println("Education:MBA in HR");
+        System.out.println("Hobby:Talk");
+        System.out.println("Address:6 Artisan Place");
+
+    }
+
+
+
+}
